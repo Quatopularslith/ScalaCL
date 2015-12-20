355626127f47a423a2df328649540c2a89969817
Creatorri <torridon@live.com>
2015-12-19 22:15:43 -0500

    stuffs

--- src/main/clojure/cl/add.clj
+++ src/main/clojure/cl/add.clj
@@ -5,7 +5,6 @@
               #^:static [add [int int] int]
               #^:static [fac [int] int]
               ]))
-(defn -add [a b]
-  (+ a b))
+(defn -add [a b] (+ a b))
 (defn fac [x] (if (= 0 x) 1 (* x (fac (dec x)))))
 (defn -fac [x] (fac x))
